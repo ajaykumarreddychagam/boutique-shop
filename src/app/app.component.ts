f5b771cc651c0f6e557314f6e3b88a01b2624d2a
Ajay Kumar Chagam <ajaychagam@gmail.com>
2025-02-10 12:35:58 +0530

    refactor: rename project from boutique-shop-9 to boutique-shop and remove unused test file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { FooterComponent } from "./footer/footer.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'boutique-shop-9';
+  title = 'boutique-shop';
 }
